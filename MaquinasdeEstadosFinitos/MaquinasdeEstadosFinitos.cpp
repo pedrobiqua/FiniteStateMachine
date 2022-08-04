bb442f356d546e4e3eb15fda60e74606f3d3a675
Pedro <pedrobiqua@gmail.com>
2022-08-04 12:34:13 -0300

    Arrumando identatação

--- MaquinasdeEstadosFinitos/MaquinasdeEstadosFinitos.cpp
+++ MaquinasdeEstadosFinitos/MaquinasdeEstadosFinitos.cpp
@@ -6,118 +6,120 @@
 #include <conio.h>
 
 enum Estados {
-    S_O,
-    S_I,
-    S_II,
-    S_III
+	S_O,
+	S_I,
+	S_II,
+	S_III
 };
 
 enum Estados estado = S_O;
 
 int lengthOfArray(const char* arr)
 {
-    int size = 0;
+	int size = 0;
 
-    while (*arr) {
-        size += 1;
-        arr += 1;
-    }
+	while (*arr) {
+		size += 1;
+		arr += 1;
+	}
 
-    return size;
+	return size;
 }
 
 int main()
 {
-    FILE* arq;
-    char Linha[100];
-    char* result;
-    int i;
-    i = 0;
+	FILE* arq;
+	char Linha[100];
+	char* result;
+	int i;
+	i = 0;
 
-    while (i < 3)
-    {
-        estado = S_O;
-        if (i == 0)
-        {
-            std::cout << "Arquivo arq1.txt" << '\n';
-            arq = fopen("arq1.txt", "rt");
-        } else if (i == 1) {
-            std::cout << '\n' << "Arquivo arq2.txt" << '\n';
-            arq = fopen("arq2.txt", "rt");
-        } else {
-            std::cout << '\n' << "Arquivo arq3.txt" << '\n';
-            arq = fopen("arq3.txt", "rt");
-        }
-        
+	while (i < 3)
+	{
+		estado = S_O;
+		if (i == 0)
+		{
+			std::cout << "Arquivo arq1.txt" << '\n';
+			arq = fopen("arq1.txt", "rt");
+		}
+		else if (i == 1) {
+			std::cout << '\n' << "Arquivo arq2.txt" << '\n';
+			arq = fopen("arq2.txt", "rt");
+		}
+		else {
+			std::cout << '\n' << "Arquivo arq3.txt" << '\n';
+			arq = fopen("arq3.txt", "rt");
+		}
 
-        if (arq == NULL) {
-            std::cout << "Problemas na abertura do arquivo" << '\n';
-            return 0;
-        }
 
-        if (arq) {
-            while (!feof(arq))
-            {
-                result = fgets(Linha, 100, arq);
-                // Se foi possível ler
-                if (result) {
-                    //TODO HERE:
-                    long lenResult = lengthOfArray(Linha);
-                    for (int j = 0; j < (lenResult - 1); j++) {
-                        switch (estado)
-                        {
-                        case S_O:
-                            if (Linha[j] == 'a')
-                            {
-                                estado = S_I;
-                            }
-                            break;
+		if (arq == NULL) {
+			std::cout << "Problemas na abertura do arquivo" << '\n';
+			return 0;
+		}
 
-                        case S_I:
-                            if (Linha[j] == 'b')
-                            {
-                                estado = S_II;
-                            }
-                            else
-                            {
-                                estado = S_III;
-                            }
-                            break;
+		if (arq) {
+			while (!feof(arq))
+			{
+				result = fgets(Linha, 100, arq);
+				// Se foi possível ler
+				if (result) {
+					//TODO HERE:
+					long lenResult = lengthOfArray(Linha);
+					for (int j = 0; j < (lenResult - 1); j++) {
+						switch (estado)
+						{
+						case S_O:
+							if (Linha[j] == 'a')
+							{
+								estado = S_I;
+							}
+							break;
 
-                        case S_II:
-                            if (Linha[j] == 'b')
-                            {
-                                estado = S_O;
-                            }
-                            else
-                            {
-                                estado = S_III;
-                            }
-                            break;
+						case S_I:
+							if (Linha[j] == 'b')
+							{
+								estado = S_II;
+							}
+							else
+							{
+								estado = S_III;
+							}
+							break;
 
-                        default:
-                            estado = S_III;
-                            break;
-                        }
-                    }
+						case S_II:
+							if (Linha[j] == 'b')
+							{
+								estado = S_O;
+							}
+							else
+							{
+								estado = S_III;
+							}
+							break;
 
-                    if (estado == S_O)
-                    {
-                        std::cout << "|PERTENCE| " << "Input: " << Linha;
-                    }
-                    else
-                    {
-                        std::cout << "|NAO PERTENCE| " << "Input: " << Linha;
-                    }
-                    estado = S_O;
+						default:
+							estado = S_III;
+							break;
+						}
+					}
 
-                }
-            }
-            fclose(arq);
+					if (estado == S_O)
+					{
+						std::cout << "|PERTENCE| " << "Input: " << Linha;
+					}
+					else
+					{
+						std::cout << "|NAO PERTENCE| " << "Input: " << Linha;
+					}
+					estado = S_O;
 
-        }
-        i++;
-    }
-    
-    return 0;
+				}
+			}
+			fclose(arq);
+
+		}
+		i++;
+	}
+
+	return 0;
 }
